4ad66d3f83782730cfa3032ee3ed4610de6a9ad2
AlexisBD <171112@ids.upchiapas.edu.mx>
2021-01-12 16:30:25 -0600

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './paginas/login/login.component';
+import { HomeComponent } from './paginas/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },  
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent },  
  
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './paginas/login/login.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './paginas/home/home.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
